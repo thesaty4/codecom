03b6932c5eeb20655194ebb609e696c33bf0cc7e
Satya Mishra <satya.mishra@bigohtech.com>
2023-12-31 23:25:31 +0530

    setup nav and hero

--- libs/codecom/portfolio/src/lib/components/hero/hero.component.ts
+++ libs/codecom/portfolio/src/lib/components/hero/hero.component.ts
@@ -1,12 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
-export class HeroComponent {
-
-}
+export class HeroComponent {}
